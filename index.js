6f475b29834f9bd902937fa64cc9656f8355a908
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2024-03-21 22:06:56 +0600

    Added text

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {});
+
+// эгерде уй тапшырма болсо
+// okurmen_js_hw_15_janara_tolonbaeva
+
+// эгерде сабак болсо
+// okurmen_js_lesson_15_janara_musakyzy
+
+// эгерде лаборотория болсо
+// okurmen_js_hw_8_janara_tolonbaeva
+
+// эгерде экзамен болсо
+// okurmen_js_exam_1_janara_tolonbaeva
